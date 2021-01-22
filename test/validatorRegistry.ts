f69e1c2dd42be4b44b43f02f3d2cb932288ccfa2
Harry Wright <hcawright@gmail.com>
2021-01-22 21:46:39 +0000

    ValidatorRegistry tests passing
    Corrected error with passing in validator address

--- test/validatorRegistry.ts
+++ test/validatorRegistry.ts
@@ -4,6 +4,7 @@ import { ethers } from "hardhat"
 import getMerkleTestData from "./data/merkleTestData"
 import type { ValidatorRegistry } from "types"
 import { hex2buf } from "../src/utils/utils"
+import { keccakFromHexString } from "ethereumjs-util"
 
 async function testFixture() {
   const { merkleTree, verifierAddresses } = await getMerkleTestData()
@@ -39,31 +40,35 @@ describe("ValidatorRegistry Contract", function () {
     it("Should correctly verify with valid proofs", async function () {
       const { validatorRegistryContract, merkleTree, verifierAddresses } = await testFixture()
 
-      const leaf = merkleTree.getLeaves()[0]
+      const validatorAddress = verifierAddresses[0]
+
+      const leaf = keccakFromHexString(validatorAddress)
       const proof = merkleTree.getProof(leaf)
       const root = hex2buf(await validatorRegistryContract.root())
 
       expect(merkleTree.verify(proof, leaf, root)).to.be.true
 
       const hexProof = merkleTree.getHexProof(leaf)
-      const senderAddress = verifierAddresses[0]
-      const result = await validatorRegistryContract.checkValidatorInSet(senderAddress, hexProof)
+
+      const result = await validatorRegistryContract.checkValidatorInSet(validatorAddress, hexProof)
 
       expect(result).to.be.true
     })
 
     it("Should not verify an invalid proof", async function () {
       const { validatorRegistryContract, merkleTree, verifierAddresses } = await testFixture()
+      const validatorAddress = verifierAddresses[0]
 
-      const leaf = merkleTree.getLeaves()[1]
+      const leaf = keccakFromHexString(validatorAddress)
       const proof = merkleTree.getProof(leaf)
       const root = hex2buf(await validatorRegistryContract.root())
 
-      expect(merkleTree.verify(proof, leaf, root)).to.be.false
+      expect(merkleTree.verify(proof, leaf, root)).to.be.true
 
       const hexProof = merkleTree.getHexProof(leaf)
-      const badSenderAddress = verifierAddresses[1]
-      const result = await validatorRegistryContract.checkValidatorInSet(badSenderAddress, hexProof)
+      const badAddress = verifierAddresses[1]
+
+      const result = await validatorRegistryContract.checkValidatorInSet(badAddress, hexProof)
 
       expect(result).to.be.false
     })
