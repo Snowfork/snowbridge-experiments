9539c659fca3fe4f3904ab6dcb9781df87b3f5c5
Philip Stanislaus <6912756+philipstanislaus@users.noreply.github.com>
2021-02-17 12:18:42 +0100

    feat: verify merkle proof at position

--- contracts/Verification.sol
+++ contracts/Verification.sol
@@ -1,6 +1,8 @@
 // "SPDX-License-Identifier: Apache-2.0"
 pragma solidity ^0.7.0;
 
+import "hardhat/console.sol";
+
 /**
  * @title Verifies various hashed data structures
  */
@@ -73,6 +75,80 @@ contract Verification {
             } else {
                 computedHash = keccak256(abi.encodePacked(proofElement, computedHash));
             }
+        }
+
+        return computedHash == root;
+    }
+
+    /**
+     * @notice Verify that a specific leaf element is part of the Merkle Tree at a specific position in the tree
+     * @dev For sake of simplifying the verification algorithm,
+     *      we make an assumption that the proof elements are sorted
+     *
+     * @param root the root of the merkle tree
+     * @param leaf the leaf which needs to be proved
+     * @param pos the position of the leaf, index starting with 0
+     * @param width the width or number of leaves in the tree
+     * @param proof the array of proofs to help verify the leafs membership, ordered from leaf to root
+     * @return a boolean value representing the success or failure of the operation
+     */
+    function verifyMerkleLeafAtPosition(
+        bytes32 root,
+        bytes32 leaf,
+        uint256 pos,
+        uint256 width,
+        bytes32[] calldata proof
+    ) public view returns (bool) {
+        console.log("root");
+        console.logBytes32(root);
+        console.log("leaf");
+        console.logBytes32(leaf);
+        console.log("pos", pos);
+        console.log("width", width);
+        bytes32 computedHash = leaf;
+
+        if (pos + 1 > width) {
+            return false;
+        }
+
+        uint256 i = 0;
+        for (uint256 height = 0; width > 1; height++) {
+            console.log("height", height);
+            console.log("  pos", pos);
+            console.log("  width", width);
+
+            bool computedHashLeft = pos % 2 == 0;
+            console.log("  computedHashLeft", computedHashLeft);
+
+            // check if at rightmost branch and whether the computedHash is left
+            if (pos + 1 == width && computedHashLeft) {
+                // there is no sibling and also no element in proofs, so we just go up one layer in the tree
+                pos /= 2;
+                width = ((width - 1) / 2) + 1;
+                continue;
+            }
+
+            if (i >= proof.length) {
+                // need another element from the proof we don't have
+                return false;
+            }
+
+            bytes32 proofElement = proof[i];
+            console.log("  proofElement");
+            console.logBytes32(proofElement);
+
+            if (computedHashLeft) {
+                computedHash = keccak256(abi.encodePacked(computedHash, proofElement));
+            } else {
+                computedHash = keccak256(abi.encodePacked(proofElement, computedHash));
+            }
+
+            console.log(" computedHash");
+            console.logBytes32(computedHash);
+
+            pos /= 2;
+            width = ((width - 1) / 2) + 1;
+            i++;
         }
 
         return computedHash == root;
