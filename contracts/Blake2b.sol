9e21d9c1e5f06ecfadda739361c5fecdc06fbea7
Philip Stanislaus <6912756+philipstanislaus@users.noreply.github.com>
2021-05-13 16:17:28 +0200

    wip

--- contracts/Blake2b.sol
+++ contracts/Blake2b.sol
@@ -1,14 +1,26 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.7.5;
 
+import "hardhat/console.sol";
+
+
 // EIP: https://eips.ethereum.org/EIPS/eip-152
 
 // blake2b on substrate uses no key, and length of 32 bytes, see https://github.com/paritytech/substrate/blob/master/primitives/core/src/hashing.rs#L42
 // the underlying implementation uses the following data: https://docs.rs/blake2-rfc/0.2.18/src/blake2_rfc/blake2.rs.html#107
 
 contract Blake2b {
+    // length of the hash in bytes
+    uint nn = 32;
+
+    // length of each data block in bytes
+    uint bb = 128;
+
+    // 12 rounds for blake2b, see https://datatracker.ietf.org/doc/html/rfc7693#section-3.2
+    uint32 rounds = 12;
+
     function f(
-        uint32 rounds,
+        uint32 r,
         bytes32[2] memory h,
         bytes32[4] memory m,
         bytes8[2] memory t,
@@ -16,7 +28,7 @@ contract Blake2b {
     ) public view returns (bytes32[2] memory) {
         bytes32[2] memory output;
 
-        bytes memory args = abi.encodePacked(rounds, h[0], h[1], m[0], m[1], m[2], m[3], t[0], t[1], last);
+        bytes memory args = abi.encodePacked(r, h[0], h[1], m[0], m[1], m[2], m[3], t[0], t[1], last);
 
         assembly {
             if iszero(staticcall(not(0), 0x09, add(args, 32), 0xd5, output, 0x40)) {
@@ -28,65 +40,118 @@ contract Blake2b {
     }
 
     // from https://datatracker.ietf.org/doc/html/rfc7693#section-3.3
-    function hash(bytes memory m) public view returns (bytes32) {
-        // uint128 cbMessageLen = length(M);
+    // Does not support a key
+    function hash(bytes calldata m) public view returns (bytes32) {
+        // length of the message in bytes
+        uint ll = m.length;
 
-        // initial state/IV
+        // number of data blocks
+        // ceil to message length / 16, but use 1 in the case that message length is 0
+        uint dd = 1 + ((ll - 1) / bb);
+
+        // initialization vector/IV
         bytes32[2] memory h;
+
+        // offset
+        bytes8[2] memory t;
+
+        // create a new hashing context
         h[0] = hex"6a09e667f3bcc908bb67ae8584caa73b3c6ef372fe94f82ba54ff53a5f1d36f1";
         h[1] = hex"510e527fade682d19b05688c2b3e6c1f1f83d9abfb41bd6b5be0cd19137e2179";
+        h[0] = h[0] ^ bytes32(hex"01010000") ^ (bytes32(nn) << (8*24));
 
-        // no key, 32 bytes hash length
-        h[0] = h[0] ^ hex"01010020";
+        // console.logBytes8(uintToBytes8(nn));
+        // console.logBytes8(bytes8(hex"01010000") ^ uintToBytes8(nn));
+        // console.logBytes32(bytes32(bytes8(hex"01010000") ^ uintToBytes8(nn)));
+        // console.logBytes32(bytes32(hex"01010000") ^ (bytes32(nn) << (8*24)));
+
         // TODO transform above into static constants
 
-        // ceil to message length / 128, but use 1 in the case that message length is 0
-        uint blocks = 1 + ((m.length - 1) / 128);
-
-        // 12 rounds for blake2b, see https://datatracker.ietf.org/doc/html/rfc7693#section-3.2
-        uint32 rounds = 12;
-
-        uint i = 0;
+        // chunks of data, having a total length of bb bytes (4 * 32 / 8 )
         bytes32[4] memory chunk;
 
-        for (; i < blocks - 1; i++) {
-            uint j = i*16;
-            chunk[0] = bytesToBytes32(concat(m[j], m[j+1], m[j+2], m[j+3]));
-            // chunk[1] = m[i*16+4:(i*16)+8];
-            // chunk[2] = m[i*16+8:(i*16)+12];
-            // chunk[3] = m[i*16+12:(i*16)+16];
-            bytes8[2] memory t = uintTo2Bytes8((i + 1) * 128);
+        console.logBytes(m);
+
+        uint i = 0;
+
+        for (; i < dd - 1; i++) {
+            uint j = i*128;
+            chunk[0] = bytesToBytes32(bytes(m[j:]));
+            console.logBytes32(chunk[0]);
+            chunk[1] = bytesToBytes32(bytes(m[j+32:]));
+            console.logBytes32(chunk[1]);
+            chunk[2] = bytesToBytes32(bytes(m[j+64:]));
+            console.logBytes32(chunk[2]);
+            chunk[3] = bytesToBytes32(bytes(m[j+96:]));
+            console.logBytes32(chunk[3]);
+            t = uintTo2Bytes8((i + 1) * bb);
             h = f(rounds, h, chunk, t, false);
         }
 
-        // chunk[0] = m[i*16:(i*16)+4];
-        // chunk[1] = m[i*16+4:(i*16)+8];
-        // chunk[2] = m[i*16+8:(i*16)+12];
-        // chunk[3] = m[i*16+12:(i*16)+16];
-        // TODO pad remainder, the above only works if we have a message that is divisible by 128
-        h = f(rounds, h, chunk, uintTo2Bytes8((i + 1) * 128), true);
+        uint j = i*128;
+
+        if (ll - j > 96) {
+            chunk[0] = bytesToBytes32(bytes(m[j:]));
+            chunk[1] = bytesToBytes32(bytes(m[j+32:]));
+            chunk[2] = bytesToBytes32(bytes(m[j+64:]));
+            chunk[3] = bytesToBytes32(bytes(m[j+96:]));
+        } else if (ll- j > 64) {
+            chunk[0] = hex"00";
+            chunk[1] = bytesToBytes32(bytes(m[j:]));
+            chunk[2] = bytesToBytes32(bytes(m[j+32:]));
+            chunk[3] = bytesToBytes32(bytes(m[j+64:]));
+        } else if (ll-j > 32) {
+            chunk[0] = hex"00";
+            chunk[1] = hex"00";
+            chunk[2] = bytesToBytes32(bytes(m[j:]));
+            chunk[3] = bytesToBytes32(bytes(m[j+32:]));
+        } else {
+            chunk[0] = hex"00";
+            chunk[1] = hex"00";
+            chunk[2] = hex"00";
+            chunk[3] = bytesToBytes32(bytes(m[j:]));
+        }
+
+        console.logBytes32(chunk[0]);
+        console.logBytes32(chunk[1]);
+        console.logBytes32(chunk[2]);
+        console.logBytes32(chunk[3]);
+
+        t = uintTo2Bytes8((i + 1) * bb);
+        h = f(rounds, h, chunk, t, true);
 
         return h[0];
     }
 
-    function concat(byte a, byte b, byte c, byte d) public pure returns (bytes memory) {
-        return abi.encodePacked(a, b, c, d);
-    }
-
-    function bytesToBytes32(bytes memory b) private pure returns (bytes32) {
+    function bytesToBytes32(bytes memory b) public pure returns (bytes32) {
         bytes32 out;
-        for (uint i = 0; i < 32; i++) {
-            out |= bytes32(b[i] & 0xFF) >> (i * 8);
+        // start from the right, effectively left-padding if less than 32 bytes in b
+        uint last = b.length;
+        if (b.length > 32) {
+            last = 32;
+        }
+
+        for (uint i = 0; i < last; i++) {
+            out |= bytes32(b[last - 1 - i] & 0xFF) >> ((32-1-i) * 8);
         }
         return out;
     }
-    function uintTo2Bytes8(uint256 i) private pure returns (bytes8[2] memory) {
+
+    // function bytesToBytes128(bytes memory b) private pure returns (bytes128) {
+    //     bytes128 out;
+    //     for (uint i = 0; i < 128; i++) {
+    //         out |= bytes128(b[i] & 0xFF) >> (i * 8);
+    //     }
+    //     return out;
+    // }
+
+    // returns the 16 last bytes of i as a fixed length array of 2 byte8 elements
+    function uintTo2Bytes8(uint256 i) public pure returns (bytes8[2] memory) {
         bytes32 b = bytes32(i);
-        // todo require b[0] and b[1] to be empty
+        // todo require other bytes to be zero?
         bytes8[2] memory out;
-        out[0] = b[2];
-        out[0] = b[3];
+        out[0] = bytes8(b << 16 * 8);
+        out[1] = bytes8(b << 24 * 8);
         return out;
     }
-
 }
--- test/blake2b.ts
+++ test/blake2b.ts
@@ -18,13 +18,53 @@ describe("Blake2b", function () {
     })
   })
 
-  describe("countSetBitsNaive", function () {
-    it("should work", async function () {
+  describe("hash", function () {
+    it.only("should work", async function () {
       // test vectors from https://github.com/BLAKE2/BLAKE2/tree/master/testvectors
 
+      // expect(
+      //   await blake2b.hash(
+      //     "0x000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f202122232425262728292a2b2c2d2e2f"
+      //   )
+      // ).to.equal("0xc7cb5d1a1a214f1d833a21fe6c7b2420e417c2f220784cbe90072975131bc367")
+      expect(await blake2b.hash("0x000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f")).to.equal(
+        "0xcb2f5160fc1f7e05a55ef49d340b48da2e5a78099d53393351cd579dd42503d6"
+      )
       expect(await blake2b.hash("0x000102030405060708090a0b0c0d0e0f")).to.equal(
         "0xc7cb5d1a1a214f1d833a21fe6c7b2420e417c2f220784cbe90072975131bc367"
       )
+    })
+  })
+
+  describe("bytesToBytes32", function () {
+    it("should work", async function () {
+      expect(await blake2b.bytesToBytes32("0x00")).to.eql(
+        "0x0000000000000000000000000000000000000000000000000000000000000000"
+      )
+      expect(await blake2b.bytesToBytes32("0x1234")).to.eql(
+        "0x0000000000000000000000000000000000000000000000000000000000001234"
+      )
+      expect(await blake2b.bytesToBytes32("0x12345678901234567890123456789012abcdefabcdefabcdefabcdefabcdefab")).to.eql(
+        "0x12345678901234567890123456789012abcdefabcdefabcdefabcdefabcdefab"
+      )
+      expect(
+        await blake2b.bytesToBytes32("0x12345678901234567890123456789012abcdefabcdefabcdefabcdefabcdefabff")
+      ).to.eql("0x12345678901234567890123456789012abcdefabcdefabcdefabcdefabcdefab")
+    })
+  })
+
+  describe("uintTo2Bytes8", function () {
+    it("should work", async function () {
+      expect(await blake2b.uintTo2Bytes8("0x00")).to.eql(["0x0000000000000000", "0x0000000000000000"])
+      expect(await blake2b.uintTo2Bytes8("0x1234")).to.eql(["0x0000000000000000", "0x0000000000001234"])
+      expect(await blake2b.uintTo2Bytes8("0x12345678901234567890123456789012")).to.eql([
+        "0x1234567890123456",
+        "0x7890123456789012",
+      ])
+      expect(await blake2b.uintTo2Bytes8("0x12345678901234567890123456789012abcdefabcdefabcdefabcdefabcdefab")).to.eql([
+        "0xabcdefabcdefabcd",
+        "0xefabcdefabcdefab",
+      ])
     })
   })
 })
