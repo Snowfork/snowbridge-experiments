8b0857c82fee8ca95ab52a67b5dd63915fde2f23
Philip Stanislaus <6912756+philipstanislaus@users.noreply.github.com>
2021-05-16 08:23:02 +0200

    fix: fix blake2b issues

--- contracts/Blake2b.sol
+++ contracts/Blake2b.sol
@@ -11,7 +11,7 @@ import "hardhat/console.sol";
 
 contract Blake2b {
     // length of the hash in bytes
-    uint nn = 32;
+    uint nn = 64;
 
     // length of each data block in bytes
     uint bb = 128;
@@ -58,32 +58,48 @@ contract Blake2b {
         // create a new hashing context
         h[0] = hex"6a09e667f3bcc908bb67ae8584caa73b3c6ef372fe94f82ba54ff53a5f1d36f1";
         h[1] = hex"510e527fade682d19b05688c2b3e6c1f1f83d9abfb41bd6b5be0cd19137e2179";
-        h[0] = h[0] ^ bytes32(hex"01010000") ^ (bytes32(nn) << (8*24));
 
-        // console.logBytes8(uintToBytes8(nn));
-        // console.logBytes8(bytes8(hex"01010000") ^ uintToBytes8(nn));
-        // console.logBytes32(bytes32(bytes8(hex"01010000") ^ uintToBytes8(nn)));
-        // console.logBytes32(bytes32(hex"01010000") ^ (bytes32(nn) << (8*24)));
+        // from blakejs
+        // h[0] = hex"f3bcc9086a09e66784caa73bbb67ae85fe94f82b3c6ef3725f1d36f1a54ff53a";
+        // h[1] = hex"ade682d1510e527f2b3e6c1f9b05688cfb41bd6b1f83d9ab137e21795be0cd19";
+
+        // The above might be wrong? According to https://github.com/axic/blake2-solidity/blob/master/contracts/Blake2b.sol#L58, it should be:
+        // instance.state = hex"0000000c08c9bdf267e6096a3ba7ca8485ae67bb2bf894fe72f36e3cf1361d5f3af54fa5d182e6ad7f520e511f6c3e2b8c68059b6bbd41fbabd9831f79217e1319cde05b00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000";
+        // h[0] = hex"08c9bdf267e6096a3ba7ca8485ae67bb2bf894fe72f36e3cf1361d5f3af54fa5";
+        // h[1] = hex"d182e6ad7f520e511f6c3e2b8c68059b6bbd41fbabd9831f79217e1319cde05b";
+        // 0x0101000000000020000000000000000000000000000000000000000000000000
+
+
+        // console.logBytes32(h[0]);
+
+        h[0] = h[0] ^ bytes32(hex"01010000") ^ (bytes32(nn) << (8*28));
+
+        // from https://eips.ethereum.org/EIPS/eip-152
+        h[0] = hex"48c9bdf267e6096a3ba7ca8485ae67bb2bf894fe72f36e3cf1361d5f3af54fa5";
+        h[1] = hex"d182e6ad7f520e511f6c3e2b8c68059b6bbd41fbabd9831f79217e1319cde05b";
+
+        // console.logBytes32(bytes32(hex"01010000") ^ (bytes32(nn) << (8*28)));
+        // console.logBytes32(h[0]);
 
         // TODO transform above into static constants
 
         // chunks of data, having a total length of bb bytes (4 * 32 / 8 )
         bytes32[4] memory chunk;
 
-        console.logBytes(m);
+        // console.logBytes(m);
 
         uint i = 0;
 
         for (; i < dd - 1; i++) {
             uint j = i*128;
             chunk[0] = bytesToBytes32(bytes(m[j:]));
-            console.logBytes32(chunk[0]);
+            // console.logBytes32(chunk[0]);
             chunk[1] = bytesToBytes32(bytes(m[j+32:]));
-            console.logBytes32(chunk[1]);
+            // console.logBytes32(chunk[1]);
             chunk[2] = bytesToBytes32(bytes(m[j+64:]));
-            console.logBytes32(chunk[2]);
+            // console.logBytes32(chunk[2]);
             chunk[3] = bytesToBytes32(bytes(m[j+96:]));
-            console.logBytes32(chunk[3]);
+            // console.logBytes32(chunk[3]);
             t = uintTo2Bytes8((i + 1) * bb);
             h = f(rounds, h, chunk, t, false);
         }
@@ -112,13 +128,25 @@ contract Blake2b {
             chunk[3] = bytesToBytes32(bytes(m[j:]));
         }
 
-        console.logBytes32(chunk[0]);
-        console.logBytes32(chunk[1]);
-        console.logBytes32(chunk[2]);
-        console.logBytes32(chunk[3]);
+        // console.logBytes32(chunk[0]);
+        // console.logBytes32(chunk[1]);
+        // console.logBytes32(chunk[2]);
+        // console.logBytes32(chunk[3]);
 
-        t = uintTo2Bytes8((i + 1) * bb);
+        // t = uintTo2Bytes8(ll);
+        t[0] = bytes8(uint64(ll));
+        t[1] = bytes8(uint64(0));
+        // TODO
+        console.log("before");
+        console.logBytes8(t[0]);
+        console.logBytes8(t[1]);
+        console.logBytes32(h[0]);
+        console.logBytes32(h[1]);
         h = f(rounds, h, chunk, t, true);
+
+        console.log("after");
+        console.logBytes32(h[0]);
+        console.logBytes32(h[1]);
 
         return h[0];
     }
